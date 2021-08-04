bb992fb664f872b10c86e51da475492af4078f38
Irina Mashkina <irinamashkina@list.ru>
2021-08-04 22:31:17 +0500

    добавить стили для компонента footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,7 +4,7 @@ function Footer() {
     return (
 <footer className="footer">
     <p className="footer__text">Учебный проект Яндекс.Практикум х BeatFilm.</p>
-    <div className="footer__conteiner">
+    <div className="footer__container">
         <p className="footer__copyright">&#169; 2021</p>
         <ul className="footer__links">
         <li><a className="footer__link" href="#" target="_blank">Яндекс.Практикум</a></li>
