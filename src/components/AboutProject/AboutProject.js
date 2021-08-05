e00b966f935e545bdc2b79488ef0d209b8d2a704
Irina Mashkina <irinamashkina@list.ru>
2021-08-05 17:12:17 +0500

    добавить стили для about-project

--- src/components/AboutProject/AboutProject.js
+++ src/components/AboutProject/AboutProject.js
@@ -1,25 +1,36 @@
 function AboutProject() {
-    return (
-        <section className="about-project">
-            <h2 className="about-project__title">О проекте</h2>
-            <div className="about-project__columns">
-                <div className="about-project__column">
-                    <h3 className="about-project__text-title">Дипломный проект включал 5 этапов</h3>
-                    <p className="about-project__text">Составление плана, работу над бэкендом, вёрстку, добавление функциональности и финальные доработки.</p>
-                </div>
-                <div className="about-project__column">
-                    <h3 className="about-project__text-title">На выполнение диплома ушло 5 недель</h3>
-                    <p className="about-project__text">У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было соблюдать, чтобы успешно защититься.</p>
-                </div>
-            </div>
+  return (
+    <section className="about-project">
+      <h2 className="about-project__title">О проекте</h2>
+      <div className="about-project__columns">
+        <div className="about-project__column">
+          <h3 className="about-project__text-title">
+            Дипломный проект включал 5 этапов
+          </h3>
+          <p className="about-project__text">
+            Составление плана, работу над бэкендом, вёрстку, добавление
+            функциональности и финальные доработки.
+          </p>
+        </div>
+        <div className="about-project__column">
+          <h3 className="about-project__text-title">
+            На выполнение диплома ушло 5 недель
+          </h3>
+          <p className="about-project__text">
+            У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было
+            соблюдать, чтобы успешно защититься.
+          </p>
+        </div>
+      </div>
 
-            <div className="about-project__duration">
-                <span className="about-project__text">Back-end</span>
-                <span className="about-project__text">Front-end</span>
-            </div>
+      <div className="about-project__duration"></div>
 
-        </section>
-    )
+      <div className="about-project__spans">
+        <span className="about-project__span">Back-end</span>
+        <span className="about-project__span">Front-end</span>
+      </div>
+    </section>
+  );
 }
 
 export default AboutProject;
