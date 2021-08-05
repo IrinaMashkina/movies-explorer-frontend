206bdc0605a71310088587456d84f08c5311656a
Irina Mashkina <irinamashkina@list.ru>
2021-08-05 14:33:30 +0500

    добавить стили для компонента Promo

--- src/components/Promo/Promo.js
+++ src/components/Promo/Promo.js
@@ -16,8 +16,8 @@ function Promo() {
           </Link>
         </ul>
       </header>
-      <div className="promo-title__container">
-        <h1 className="promo-title">
+      <div className="promo-title">
+        <h1 className="promo-title__text">
           Учебный проект студента факультета Веб-разработки.
         </h1>
       </div>
