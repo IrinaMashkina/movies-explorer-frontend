1dacf94075c50260cf39da7ebb2b05e5ff223d21
Irina Mashkina <irinamashkina@list.ru>
2021-08-12 11:36:10 +0500

    добавить адаптив Profile

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,27 +1,31 @@
 import React from "react";
 
-function Profile(props) {
+function Profile() {
   return (
     <section className="profile">
-      <h2 className="profile__title">Привет, имя_пользователя</h2>
-      <div className="profile__info">
-        <div className="profile__info-container">
-          <p className="profile__info-title">Имя</p>
-          <p className="profile__info-text">имя_пользователя</p>
-        </div>
+      <div className="profile__container">
+        <div className="profile__info">
+          <h2 className="profile__title">Привет, имя_пользователя</h2>
+          <div className="profile__grid-container">
+            <div className="profile__info-container">
+              <p className="profile__info-title">Имя</p>
+              <p className="profile__info-text">имя_пользователя</p>
+            </div>
 
-        <div className="profile__info-container">
-          <p className="profile__info-title">E-mail</p>
-          <p className="profile__info-text">email_пользователя</p>
+            <div className="profile__info-container">
+              <p className="profile__info-title">E-mail</p>
+              <p className="profile__info-text">email_пользователя</p>
+            </div>
+          </div>
         </div>
-      </div>
-      <div className="profile__buttons">
-        <button type="button" className="profile__button-edit">
-          Редактировать
-        </button>
-        <button type="button" className="profile__button-exit">
-          Выйти из аккаунта
-        </button>
+        <div className="profile__buttons">
+          <button type="button" className="profile__button-edit">
+            Редактировать
+          </button>
+          <button type="button" className="profile__button-exit">
+            Выйти из аккаунта
+          </button>
+        </div>
       </div>
     </section>
   );
