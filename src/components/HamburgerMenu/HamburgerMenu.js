94d443d95344a41effdde1dd7baef24de750d3cc
Irina Mashkina <irinamashkina@list.ru>
2021-08-17 11:45:50 +0500

    исправить гамбургер-меню

--- src/components/HamburgerMenu/HamburgerMenu.js
+++ src/components/HamburgerMenu/HamburgerMenu.js
@@ -2,30 +2,32 @@ import React from "react";
 import PopupMenu from "../PopupMenu/PopupMenu";
 
 function HamburgerMenu() {
+  const [hamburgerMenuIsOpen, setHamburgerMenuIsOpen] = React.useState(false);
 
+  function handleToggleHamburgerMenu() {
+    setHamburgerMenuIsOpen(!hamburgerMenuIsOpen);
+  }
 
-    const [hamburgerMenuIsOpen, setHamburgerMenuIsOpen] = React.useState(false);
-
-    function handleToggleHamburgerMenu() {
-      setHamburgerMenuIsOpen(!hamburgerMenuIsOpen);
-    }
-  
-
-    
-    return (
-        <>
-      { !hamburgerMenuIsOpen && (
-        <button  onClick={handleToggleHamburgerMenu} className="hamburger-menu" type="button">
+  return (
+    <>
+      {!hamburgerMenuIsOpen && (
+        <button
+          onClick={handleToggleHamburgerMenu}
+          className="hamburger-menu"
+          type="button"
+        >
           <div className="hamburger-menu-line"></div>
-        </button>)
-     }
+        </button>
+      )}
 
-
-     {hamburgerMenuIsOpen && (
-         <PopupMenu toggleMenu={handleToggleHamburgerMenu} isOpen={hamburgerMenuIsOpen}/>
-     )}
-     </>
-    )
+      {hamburgerMenuIsOpen && (
+        <PopupMenu
+          toggleMenu={handleToggleHamburgerMenu}
+          isOpen={hamburgerMenuIsOpen}
+        />
+      )}
+    </>
+  );
 }
 
 export default HamburgerMenu;
