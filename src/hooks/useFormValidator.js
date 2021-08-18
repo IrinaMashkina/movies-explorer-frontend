d092fc26f6555605f1ca97fad42ad8674b54dba7
Irina Mashkina <irinamashkina@list.ru>
2021-08-18 18:19:12 +0500

    создать FormValidator

---
+++ src/hooks/useFormValidator.js
@@ -0,0 +1,35 @@
+import { useCallback, useState } from "react";
+
+function useFormValidator() {
+  const [inputValues, setInputValues] = useState({});
+  const [errorMessages, setErrorMessages] = useState({});
+  const [isValid, setIsValid] = useState(false);
+
+  function handleInputChange(e) {
+console.log(e.target.closest(".forms").checkValidity())
+    setInputValues({
+      ...inputValues,
+      [e.target.name]: e.target.value,
+    });
+    setErrorMessages({
+      ...errorMessages,
+      [e.target.name]: e.target.validationMessage,
+    });
+    
+    setIsValid(e.target.closest(".forms").checkValidity());
+  }
+
+  const resetForm = useCallback(
+    (newValues = {}, newErrors = {}, newIsValid = false) => {
+      setInputValues(newValues);
+      setErrorMessages(newErrors);
+      setIsValid(newIsValid);
+    },
+    [setInputValues, setErrorMessages, setIsValid]
+  )
+
+  return { inputValues, errorMessages, isValid, handleInputChange, resetForm };
+
+}
+
+export default useFormValidator;
